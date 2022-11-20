667884aa715994768eace43ae28106ab389cb5a2
idrisabdul <idrisaziz52@gmail.com>
2022-11-20 10:21:55 +0700

    fix vendor

--- application/views/history/v_history.php
+++ application/views/history/v_history.php
@@ -16,11 +16,11 @@
   <div class="container-fluid">
     <div class="row mb-2">
       <div class="col-sm-6">
-        <h1>Profile</h1>
+        <h1>Detail Asset</h1>
       </div>
       <div class="col-sm-6">
         <ol class="breadcrumb float-sm-right">
-          <li class="breadcrumb-item"><a href="#">Home</a></li>
+          <li class="breadcrumb-item"><a href="https://hipernet.bumenet.com">Home</a></li>
           <li class="breadcrumb-item active">Detail Asset</li>
         </ol>
       </div>
--- application/views/vendor/v_vendor.php
+++ application/views/vendor/v_vendor.php
@@ -90,7 +90,7 @@
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h4 class="title" id="defaultModalLabel">Edit Lantai</h4>
+                <h4 class="title" id="defaultModalLabel">Edit Vendor</h4>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
